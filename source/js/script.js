ed07d8f385e8938b335fd8d6bf90bbf1ef6c1489
MedeAnaCode <medentsova.anastasia@yandex.ru>
2023-01-22 16:44:32 +0300

    Добавляет карту без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,14 @@
 let mainHeader = document.querySelector('.main-header');
 let navToggle = document.querySelector('.main-header__toggle');
 let navigation = document.querySelector('.main-header__nav');
+let locationImage = document.querySelector('.location__map-image');
+let locationMap = document.querySelector('.location__map');
 
 mainHeader.classList.remove('main-header--nojs');
 navToggle.classList.remove('main-header__toggle--nojs');
 navigation.classList.remove('main-header__nav--nojs');
+locationImage.classList.remove('location__map-image--nojs');
+locationMap.classList.remove('location__map--nojs');
 
 navToggle.addEventListener('click', function () {
     navigation.classList.toggle('main-header__nav--closed');
