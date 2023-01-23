5a4a341a810a52f1255fe6dcc30e68e19c268b3a
MedeAnaCode <medentsova.anastasia@yandex.ru>
2023-01-23 18:14:16 +0300

    Добавляет правки после первой попытки

--- source/js/script.js
+++ source/js/script.js
@@ -4,12 +4,14 @@ let navigation = document.querySelector('.main-header__nav');
 let locationImage = document.querySelector('.location__map-image');
 let locationMap = document.querySelector('.location__map');
 
+/* Отлючение режима nojs */
 mainHeader.classList.remove('main-header--nojs');
 navToggle.classList.remove('main-header__toggle--nojs');
 navigation.classList.remove('main-header__nav--nojs');
 locationImage.classList.remove('location__map-image--nojs');
 locationMap.classList.remove('location__map--nojs');
 
+/* Открытие и закрытие мобильного меню */
 navToggle.addEventListener('click', function () {
     navigation.classList.toggle('main-header__nav--closed');
     navigation.classList.toggle('main-header__nav--opened');
